143ac062d73dec2fe9ac5d087b4fa8dc67a3e45b
Kevin Xie <kevin.xie@dtcpay.com>
2024-05-22 14:02:17 +0800

    UPDATE: version 1.0

--- src/serialization.rs
+++ src/serialization.rs
@@ -4,9 +4,9 @@
 pub fn serialize() {
 
     let target: Option<String> = Some("hello_world".to_string());
-
-    let encoded: Vec<u8> = bincode::serialize(&target).unwrap();
-
-    let decoded: Option<String> = bincode::deserialize(&encoded[..]).unwrap();
-    assert_eq!(target, decoded);
+    //
+    // let encoded: Vec<u8> = bincode::serialize(&target).unwrap();
+    //
+    // let decoded: Option<String> = bincode::deserialize(&encoded[..]).unwrap();
+    // assert_eq!(target, decoded);
 }
